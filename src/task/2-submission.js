821d29e50e04646e80ca7992e4aa640e9d5960a7
Hakiki Tech <57021097+hakikicode@users.noreply.github.com>
2024-11-25 12:12:52 +0100

    Update 2-submission.js

--- src/task/2-submission.js
+++ src/task/2-submission.js
@@ -3,8 +3,8 @@ import { namespaceWrapper } from "@_koii/namespace-wrapper";
 export async function submission(roundNumber) {
   try {
     console.log(`Submitting data for round ${roundNumber}`);
-    const gameData = await namespaceWrapper.storeGet(`round_${roundNumber}_gameData`);
-    return gameData;
+    const gameData = await namespaceWrapper.storeGet(`round_${roundNumber}_fortniteLeaderboard`);
+    return gameData; // Return stored data for validation/auditing
   } catch (error) {
     console.error("Submission error:", error);
   }
