6a122bc45e69fe7c5a70338fcbf86f8812a1c646
Hakiki Tech <hakikicode@gmail.com>
2024-12-05 21:17:30 +0100

    final

--- tests/config.js
+++ tests/config.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 
 export const TASK_ID =
-  process.env.TASK_ID || "BXbYKFdXZhQgEaMFbeShaisQBYG1FD4MiSf9gg4n6mVn";
+  process.env.TASK_ID || "7bBPKJQkzHzKDtHMXBmA5RAo7UqemDLSjBpgbWi2VS8p";
 export const WEBPACKED_FILE_PATH =
   process.env.WEBPACKED_FILE_PATH || "../dist/main.js";
 
@@ -9,4 +9,4 @@ const envKeywords = process.env.TEST_KEYWORDS ?? "";
 
 export const TEST_KEYWORDS = envKeywords
   ? envKeywords.split(",")
-  : ["TEST", "EZ TESTING"];
+  : ["TEST", "FN TESTING"];
